5f3bfeb28107b060b42d9fe50eabd1da58a43038
nitishbhosale7 <nitishbhosale7@gmail.com>
2025-04-26 22:57:12 +0530

    testing

--- app/main.py
+++ app/main.py
@@ -14,26 +14,23 @@ def main():
     request = conn.recv(1024).decode('utf-8')
     print("request",request)
     print("request split",request.split("\r\n"))
-    headerInfoValue = request.split("\r\n")[2].split(": ")[1]
     url_path = request.split(" ")[1]
-    print("headerInfoValue",headerInfoValue)
-    # if url_path.startswith("/echo/"):
-    #     endpoint = url_path.split("/")[2]
-    #     print("endpoint",endpoint)
-    #     _response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:{len(endpoint)}\r\n\r\n{endpoint}"
-    #     response =  _response.encode('utf-8')
-    # elif url_path == "/":
-    #     response = b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 0\r\n\r\n"
-    # else:
-    #     response = b"HTTP/1.1 404 Not Found\r\n\r\n"
-
-    if url_path.startswith("/user-agent"):
+    if url_path.startswith("/echo/"):
+        endpoint = url_path.split("/")[2]
+        print("endpoint",endpoint)
+        _response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:{len(endpoint)}\r\n\r\n{endpoint}"
+        response =  _response.encode('utf-8')
+    elif url_path.startswith("/user-agent"):
+        headerInfoValue = request.split("\r\n")[2].split(": ")[1]
+        print("headerInfoValue",headerInfoValue)
         _response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:{len(headerInfoValue)}\r\n\r\n{headerInfoValue}"
         response =  _response.encode('utf-8')
     elif url_path == "/":
         response = b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 0\r\n\r\n"
     else:
         response = b"HTTP/1.1 404 Not Found\r\n\r\n"
+
+    
             
     conn.sendall(response)
     conn.close()
