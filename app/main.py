2e71905fb034a6664f208793f0a4f3613641f47a
nitishbhosale7 <nitishbhosale7@gmail.com>
2025-04-26 14:25:40 +0530

    Respond with 200

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()[0].sendall('HTTP/1.1 200 OK\r\n\r\n'); # wait for client
+    server_socket.accept()[0].sendall(b'HTTP/1.1 200 OK\r\n\r\n'); # wait for client
     
 
 
